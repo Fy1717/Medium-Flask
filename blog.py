14ea8e00731a3701b66a4c84e96ba3566dc5c9e3
Fy1717 <frknyldz8489@gmail.com>
2020-08-25 00:30:08 +0300

    added (if-for loop, blocks-extends, about-navbar-layout pages)

--- blog.py
+++ blog.py
@@ -1,23 +1,49 @@
-from flask import Flask, render_template
+#!/usr/local/bin/python
+
+#- * -coding: utf - 16 - * -
+
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    """user = dict()
-    user["name"] = "Furkan"
-    user["surname"] = "YILDIZ"
-    user["age"] = 23
-    user["nick"] = "Punisher"
+    educations = ["Flutter", "Python", "Xamarin", "Javascript", "Firebase"]
 
-    return render_template('index.html', user=user)
-    """
+    instructors = [{
+        "id": 1,
+        "name": "Furkan YILDIZ",
+        "profession": "PYTHON"
+    }, {
+        "id": 2,
+        "name": "Kadriye MACIT",
+        "profession": "FLUTTER"
+    }, {
+        "id": 3,
+        "name": "Beyza INCE",
+        "profession": "ARTIFICAL INTELLIGENCE"
+    }, {
+        "id": 4,
+        "name": "Oguz DEMIR",
+        "profession": "JAVASCRIPT"
+    }, {
+        "id": 5,
+        "name": "Alp YURTSEVEN",
+        "profession": "ASP .NET"
+    }]
 
-    return render_template('index.html')
+    return render_template("index.html", instructors=instructors, educations=educations)
 
 @app.route("/about")
 def about():
-    return "About"
+    return render_template("about.html")
+
+@app.route("/instructors/<string:id>")
+def detail(id):
+    return "Instructor " + id 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
