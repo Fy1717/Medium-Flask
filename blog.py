ecca65184cf25502d637343ca515138c83b353bf
Fy1717 <frknyldz8489@gmail.com>
2020-09-05 20:47:29 +0300

    register, login, educations default sayfaları eklendi

--- blog.py
+++ blog.py
@@ -4,10 +4,18 @@
 
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-from passlib.hash import sha256_crypt
+#from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+#from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "hwblog"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
@@ -40,6 +48,18 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/educations")
+def educations():
+    return render_template("educations.html")
 
 @app.route("/instructors/<string:id>")
 def detail(id):
